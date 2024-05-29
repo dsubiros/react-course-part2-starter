9b06fd13f2f424543f16b070b048cf1bf8b18257
Dariel Subiros <dsubiros@gmail.com>
2024-05-28 23:38:09 -0400

    Extract custom hook for Posts

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  // defaultOptions: {
+  //   queries: {
+  //     retry: 3, 
+  //     cacheTime: 300_000, //5m
+  //     staleTime: 10 * 1000, //10s
+      
+  //     refetchOnWindowFocus: false,
+  //     refetchOnReconnect: false,
+  //     refetchOnMount: false,
+  //   }
+  // }
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
