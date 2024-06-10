2485df5f730cd300180f3aba575d832ff0dc128f
Dariel Subiros <dsubiros@gmail.com>
2024-06-09 22:58:29 -0400

    Refactor- create custom Provider

--- src/App4.tsx
+++ src/App4.tsx
@@ -1,25 +1,23 @@
 import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
 import AuthContext from "./state-management/contexts/authContext";
 import TasksContext from "./state-management/contexts/tasksContext";
 import HomePage from "./state-management/HomePage";
-import LoginStatus from "./state-management/LoginStatus";
 import NavBar from "./state-management/NavBar";
 import authReducer from "./state-management/reducers/authReducer";
 import tasksReducer from "./state-management/reducers/tasksReducer";
 
 function App4() {
-  const [user, authDispatch] = useReducer(authReducer, "");
-
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 
   // return (
