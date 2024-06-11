ea28503b5c02c1ec1a6ba95f272f974bc54a2f1f
Dariel Subiros <dsubiros@gmail.com>
2024-06-11 00:05:49 -0400

    Refactor- re-render only when needed props change

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -7,13 +7,17 @@ const NavBar = () => {
   const { tasks } = useTasks();
   // const { user } = useAuth();
   const { user } = useAuthStore();
-  const {counter} = useCounterStore();
+  const max = useCounterStore(s => s.max);
+  
+  console.log('Render NavBar');
+  
 
   return (
     <nav className="navbar d-flex justify-content-between">
       {!!user && (
         // <span className="badge text-bg-secondary">{tasks.length}</span>
-        <span className="badge text-bg-secondary">{counter}</span>
+        // <span className="badge text-bg-secondary">{counter}</span>
+        <span className="badge text-bg-secondary">{max}</span>
       )}
       <LoginStatus />
     </nav>
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,21 +1,28 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
   counter: number;
   increment: () => void;
   reset: () => void;
+  max: number;
 }
 
 const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
+  max: 5,
   increment: () =>
     set((store) => ({
       counter: store.counter + 1,
     })),
   reset: () =>
     set(() => ({
-      counter: 0,
+      max: 10,
+      // counter: 0,
     })),
 }));
+
+if (process.env.NODE_ENV === "development")
+  mountStoreDevtool("Counter Store", useCounterStore);
 
 export default useCounterStore;
